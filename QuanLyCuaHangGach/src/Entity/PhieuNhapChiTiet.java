511995a215d93185ae8e246d40bcfbd1216d7f39
hoangnvph17059 <73219380+hoangnvph17059@users.noreply.github.com>
2021-11-22 00:33:59 +0700

    VietHoang_SuaEntity_PhieuNhapChiTiet

--- QuanLyCuaHangGach/src/Entity/PhieuNhapChiTiet.java
+++ QuanLyCuaHangGach/src/Entity/PhieuNhapChiTiet.java
@@ -66,6 +66,9 @@ public class PhieuNhapChiTiet {
         return GiaBan;
     }
 
+    public PhieuNhapChiTiet() {
+    }
+
     public void setGiaBan(float GiaBan) {
         this.GiaBan = GiaBan;
     }
