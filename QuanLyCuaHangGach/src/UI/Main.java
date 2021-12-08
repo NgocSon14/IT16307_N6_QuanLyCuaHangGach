6f35ba799dd5dc3462e57b390e59c5def6ad2142
Ngọc Sơn <80674946+NgocSon14@users.noreply.github.com>
2021-12-08 17:05:49 +0700

    Sondnph17102_Thống kê doanh Thu biểu đồ (#82)

--- QuanLyCuaHangGach/src/UI/Main.java
+++ QuanLyCuaHangGach/src/UI/Main.java
@@ -322,7 +322,7 @@ public class Main extends javax.swing.JFrame {
     private void jLabel8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseClicked
         if (tkdt == null) {
             tkdt = new ThongKeDoanhThu();
-            tabToTab.addTab("Thống kê doanh thu", tkdt);
+            tabToTab.addTab("Quản lí đọc giả", tkdt);
         }
         tabToTab.setSelectedComponent(tkdt);
     }//GEN-LAST:event_jLabel8MouseClicked
