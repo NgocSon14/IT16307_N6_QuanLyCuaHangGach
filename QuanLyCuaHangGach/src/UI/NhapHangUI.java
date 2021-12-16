228cef81fce9d948be31590d82e967c5fced1862
NgocSon14 <80674946+NgocSon14@users.noreply.github.com>
2021-12-16 20:22:37 +0700

    Sondnph17102_SuaPhanCheckDuLieu
    Sửa phần check dữ liệu

--- QuanLyCuaHangGach/src/UI/NhapHangUI.java
+++ QuanLyCuaHangGach/src/UI/NhapHangUI.java
@@ -755,11 +755,10 @@ public class NhapHangUI extends javax.swing.JFrame {
     }//GEN-LAST:event_tblPhieuNhapMouseClicked
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        if (Check1.checkName(txttenncc)
-                &&Check1.checkNullText(txtdiachi)
-                &&Check1.checkEmail(txtemail)
-                && Check1.checkSDT(txtsdt)
-                )
+//        if (Check1.checkName(txtNhaCungCap)
+//                &&Check1.checkEmail(txtemail)
+//                && Check1.checkSDT(txtsdt)
+//                )
         ThemPn();
     }//GEN-LAST:event_jButton3ActionPerformed
 
